ae5a31cf56f1359075b671a75d5b7834f5747771
Alexander Luyo <alexander.valusk@gmail.com>
2025-06-20 14:25:39 -0500

    Proyecto Inicial

---
+++ Grupo_1/script.js
@@ -0,0 +1,23 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slider = document.getElementById("slider");
+    const slides = slider.children;
+    if (index >= slides.length) currentIndex = 0;
+    else if (index < 0) currentIndex = slides.length - 1;
+    else currentIndex = index;
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+// Auto cambiar cada 4 segundos
+setInterval(() => {
+    nextSlide();
+}, 4000);
